14eacb4c13c4ab6aa0a485973c11e5a24be32fb1
Unnati Gandhi <gandhiunnati611@gmail.com>
2023-11-07 08:43:27 +0530

    login page

---
+++ practice.js
@@ -0,0 +1,14 @@
+// let a = document.getElementById("user").value
+// let b = document.getElementById("password").value
+function myfunction() {
+    let a = document.getElementById("user").value
+    let b = document.getElementById("password").value
+    
+  if (a === "gandhiunnati611"  && b === "61198") {
+      window.location.href ="https://www.google.com/"
+  } 
+  else {
+      alert("try again")
+  }
+  
+   }
